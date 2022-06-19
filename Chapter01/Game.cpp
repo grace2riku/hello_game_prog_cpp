bc46b9f1eb8e30becfd93e7dd050eeef6876ad3a
k_abe <gracehime@gmail.com>
2022-06-19 15:43:05 +0900

    p86-87 パドルとボール・壁右側の衝突処理を実装した。ボールが左の壁から外にでた場合はゲームを終了する。

--- Chapter01/Game.cpp
+++ Chapter01/Game.cpp
@@ -131,6 +131,30 @@ void Game::UpdateGame() {
     mBallPos.x += mBallVel.x * deltaTime;
     mBallPos.y += mBallVel.y * deltaTime;
 
+    // Bounce if needed
+    // Did we intersect with paddle?
+    float diff = mPaddlePos.y - mBallPos.y;
+    // Take absolute value of difference
+    diff = (diff > 0.0f) ? diff: -diff;
+    if (
+        // Our y-difference is small enough
+        diff <= paddleH / 2.0f &&
+        // We are in the correct x-position
+        mBallPos.x <= 25.0f && mBallPos.x >= 20.0f &&
+        // The ball is moving to the left
+        mBallVel.x < 0.0f)
+    {
+        mBallVel.x *= -1.0f;
+    }
+    // Did thr ball go off the screen? (if so, end game)
+    else if (mBallPos.x <= 0.0f) {
+        mlsRunning = false;
+    }
+    // Did the ball collide with the right wall?
+    else if (mBallPos.x >= (1024.0f - thickness) && mBallVel.x > 0.0f) {
+        mBallVel.x *= -1.0f;
+    }
+
     // Did the ball collide with the top wall?
     if (mBallPos.y <= thickness && mBallVel.y < 0.0f) {
         mBallVel.y *= -1;
